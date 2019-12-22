c7fd560f810ecd1de7938a62a6999df4e09c7aaf
Bruno Figueiredo <brunocfigueiredo@tecnico.ulisboa.pt>
2019-12-22 01:48:56 +0000

    postal code, maybe done

--- postctt.py
+++ postctt.py
@@ -3,45 +3,73 @@ import requests as req
 import json
 import sys
 from lxml import html
-from asterisk.agi import * 
+from asterisk.agi import * #you need to install pyst2 
 
 #agi = AGI()
+
+def set_var(name_var, value):    
+    sys.stdout.write("SET VARIABLE %s %s \"\"\n" %(name_var,value)) 
+    sys.stdout.flush()
+    sys.stdout.write("SET VARIABLE %s %s \"\"\n" %(name_var,value)) 
+    sys.stdout.flush()
+    pass 
+
+asterisk_env = {}
+code1=""
+code2=""
+
 try:
-	code1=str(sys.argv[1]) 
-	code2=str(sys.argv[2])
-	#code1 = ''
-	#code2 = ''
-	#agi.verbose(code1)
+    code1=str(sys.argv[1]) 
+    code2=str(sys.argv[2])
 
-	url = "https://www.ctt.pt/feapl_2/app/open/postalCodeSearch/postalCodeSearch.jspx?cp4="+code1+"&cp3="+code2+"&method:searchPC2=Procurar"
+except: 
+    
+    while True:
+        line = sys.stdin.readline().strip()         
+        if not len(line):
+            break
+        try:
+            var_name,var_value = line.split(':',1)
+            asterisk_env[var_name] = var_value 
+        except:
+            pass 
 
-	url2 =  "https://www.ctt.pt/feapl_2/app/open/postalCodeSearch/postalCodeSearch.jspx?cp4=1000&cp3=048&method:searchPC2=Procurar"
-
-	page = req.post(url2)
-	tree = html.fromstring(page.content)
-	info1 = tree.xpath('//h4[@class="subheader"]/text()')
-	info2 = tree.xpath('//h3[@class="subheader"]/text()')
-
-
-	string = "A" 
-	for a in info1:
-    		string += a + " "
-	for a in info2:
-    		string += a + " "
+    try: 
+        code1 = str(asterisk_env["num1"])
+        code2 = str(asterisk_env["num2"])
+    except:
+        pass 
 
 
-	#print("SET VARIABLE STRING A" + string)
-	#agi.set_variable('STRING', code1)
-	sys.stdout.write("SAY NUMBER %s \"\"\n" %'2') 
-	sys.stdout.flush()
-	sys.stdout.write("SAY NUMBER %s \"\"\n" %'2') 
-        sys.stdout.flush()
+
+try:
+
+    try:
+        #code1 = agi.get_variable('num1')
+        #code2 = agi.get_variable('num2')
+        pass 
+    except:
+        pass
+    
+    url = "https://www.ctt.pt/feapl_2/app/open/postalCodeSearch/postalCodeSearch.jspx?cp4="+code1+"&cp3="+code2+"&method:searchPC2=Procurar"
+    page = req.post(url)
+    tree = html.fromstring(page.content)
+    info1 = tree.xpath('//h4[@class="subheader"]/text()')
+    info2 = tree.xpath('//h3[@class="subheader"]/text()')
+
+    # concatenation 
+    string = "" 
+    for a in info1:
+        string += a + " "
+    for a in info2:
+        string += a + " "
+
+
+
+    #agi.set_variable('str', string)
+    set_var('str', string)
 
 except:
- 	sys.stdout.write("SAY NUMBER %s \"\"\n" %'2') 
-        sys.stdout.flush()
-        sys.stdout.write("SAY NUMBER %s \"\"\n" %'2') 
-        sys.stdout.flush()
+    pass
 
-	pass
 
